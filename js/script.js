e9dbb8fee100580b15eddc007d456aae0bd80517
serabi patient relation <amiraputrik@gmail.com>
2023-10-29 22:21:14 +0700

    bikin jadi focus

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ const searchBox = document.querySelector("#search-box");
 
 document.querySelector("#search-button").onclick = () => {
   searchForm.classList.toggle("active");
+  searchBox.focus();
 };
 
 //klik diluar sidebar untuk menghilangkan nav
