17601f49b36c59d34c65bf224ea8efc9ee058a7a
serabi patient relation <amiraputrik@gmail.com>
2023-10-29 22:28:25 +0700

    biar ga bergulir ke atas

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ document.querySelector("#hamburger-menu").onclick = () => {
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
-document.querySelector("#search-button").onclick = () => {
+document.querySelector("#search-button").onclick = (e) => {
   searchForm.classList.toggle("active");
   searchBox.focus();
+  e.preventDefault();
 };
 
 //klik diluar sidebar untuk menghilangkan nav
