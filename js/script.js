453f6878a5dda0f17052dcd27511ef4baa28c2d4
serabi patient relation <amiraputrik@gmail.com>
2023-10-29 22:44:16 +0700

    untuk konfigurasi search button

--- js/script.js
+++ js/script.js
@@ -15,11 +15,16 @@ document.querySelector("#search-button").onclick = (e) => {
   e.preventDefault();
 };
 
-//klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+//klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
